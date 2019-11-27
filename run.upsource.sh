e19797f5f6d13a4dd87f8625e3c44d2cddc04c74
Vitawt-The-Duke <53478625+Vitawt-The-Duke@users.noreply.github.com>
2019-11-27 15:50:02 +0300

    Update run.upsource.sh

--- run.upsource.sh
+++ run.upsource.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-UPSOURCEVER="2019.1.1556"
+UPSOURCEVER="$3"
 REMOTEHOST="$1"
 EXTPORT="$2"
 ####################################
-mkdir /upsource
-cd /upsource
-mkdir -p -m 750 data logs conf backups
-ufw allow from $"REMOTEHOST"/32 to any port $"EXTPORT"
-docker run -it --name upsource1 --memory=7g \
+#mkdir /upsource
+#cd /upsource
+#mkdir -p -m 750 data logs conf backups
+ufw allow from "$REMOTEHOST"/32 to any port "$EXTPORT"
+docker run --name upsource-$(date +"%m:%S_%d_%Y") --memory=7g -d  \
 -v /upsource/data:/opt/upsource/data \
 -v /upsource/conf:/opt/upsource/conf \
 -v /upsource/logs:/opt/upsource/logs \
--v /upsource/packups:/opt/upsource/backups \
--p $"EXTPORT":8080 \
-jetbrains/upsource:jetbrains/upsource:$"UPSOURCEVER"
+-v /upsource/backups:/opt/upsource/backups \
+-p "$EXTPORT":8080 \
+jetbrains/upsource:"$UPSOURCEVER"
